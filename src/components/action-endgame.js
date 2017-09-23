71eb3dc530ae9ed1fa90ccb1adc38f849cba2956
Chris Richards <ripter001@gmail.com>
2017-09-23 16:40:47 -0700

    base new component

---
+++ src/components/action-endgame.js
@@ -0,0 +1,36 @@
+import eventToMethod from '../utils/eventToMethod.js';
+
+AFRAME.registerComponent('action-endgame', {
+  schema: {
+    onEvents: {default: ['click', 'alongpath-trigger-activated']},
+  },
+
+  /**
+   * Called once at the beginning of the component’s lifecycle
+   * reference: https://aframe.io/docs/0.6.0/core/component.html#init
+   */
+  init() {
+    const { el } = this;
+    const { onEvents } = this.data;
+
+    onEvents.forEach((eventName) => {
+      el.addEventListener(eventName, this);
+    });
+  },
+  /**
+   * Called whenever the component is detached from the entity
+   * reference: https://aframe.io/docs/0.6.0/core/component.html#remove
+   */
+  remove() {
+    const { el } = this;
+    const { onEvents } = this.data;
+
+    onEvents.forEach((eventName) => {
+      el.removeEventListener(eventName, this);
+    });
+  },
+
+  handleEvent(event) {
+    console.log('action-endgame', event.type, event);
+  }
+});
